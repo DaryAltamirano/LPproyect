1a71fd739017f8c9b7b6745e0d9b6cc427f1a2e8
abbarco <68168827+abbarco@users.noreply.github.com>
2021-02-01 23:19:03 -0500

    Se crean url desaparecido

--- LPDenuncia/frontend/src/router/index.js
+++ LPDenuncia/frontend/src/router/index.js
@@ -6,6 +6,10 @@ import editdenunciante from '@/components/AppDenuncia/editdenunciante'
 import deletedenunciante from '@/components/AppDenuncia/deletedenunciante'
 import newdenunciante from '@/components/AppDenuncia/newdenunciante'
 import Home from '@/components/AppDenuncia/Home'
+import desaparecido from '@/components/AppDenuncia/desaparecido'
+import editdesaparecido from '@/components/AppDenuncia/editdenunciante'
+import deletedesaparecido from '@/components/AppDenuncia/deletedesaparecido'
+import newdesaparecido from '@/components/AppDenuncia/newdesaparecido'
 Vue.use(Router)
 export default new Router({
   routes: [
@@ -29,6 +33,22 @@ export default new Router({
       path: '/denunciante/crear',
       name: 'newdenunciante',
       component: newdenunciante
+    },{
+      path: '/desaparecido',
+      name: 'desaparecido',
+      component: desaparecido
+    },{
+      path: '/desaparecido/:desaparecidoId/edit/',
+      name: 'editdesaparecido',
+      component: editdesaparecido
+    },{
+      path: '/desaparecido/:desaparecidoId/eliminar',
+      name: 'deletedesaparecido',
+      component: deletedesaparecido
+    },{
+      path: '/desaparecido/crear',
+      name: 'newdesaparecido',
+      component: newdesaparecido
     },
   ],
   mode : 'history'
